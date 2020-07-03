469ec9f70768726a1601a10619cb3156b14bb14e
ueihgnurt <aikawa.yuuto@gmail.com>
2020-07-03 14:31:35 +0700

    chapter_9 full

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,7 +4,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:hieu)
   end
-
+  
   test "login with valid/invalid password" do
     get login_path
     assert_template "sessions/new"
@@ -24,16 +24,47 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
-    assert_template 'users/show'
+    assert_template "users/show"
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "login with remembering" do
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: "123456",
+        remember_me: "1"
+        }
+      }
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: "123456",
+        remember_me: "1"
+        }
+      }
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: "123456",
+        remember_me: "0"
+        }
+      }
+    assert_empty cookies[:remember_token]
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
 class ActiveSupport::TestCase
-  parallelize(workers: :number_of_processors)
   fixtures :all
   def is_logged_in?
     !session[:user_id].nil?
   end
+
 end
