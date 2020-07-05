eb1c9df5d89b2f588618c7a52312ad8130e47c52
ueihgnurt <aikawa.yuuto@gmail.com>
2020-07-05 09:47:58 +0700

    o

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,9 @@
 class StaticPagesController < ApplicationController
   def home; end
 
-<<<<<<< HEAD
-  def help
-  end
-=======
   def help; end
 
   def about; end
 
   def contact; end
->>>>>>> 90b715d... o
 end
