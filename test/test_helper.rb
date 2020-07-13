f4f7890acec2dc73cbc1a7197269b561fbb53331
ueihgnurt <aikawa.yuuto@gmail.com>
2020-07-13 13:40:57 +0700

    chapter_10 full

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,18 @@ require "rails/test_help"
 class ActiveSupport::TestCase
   fixtures :all
   def is_logged_in?
-    !session[:user_id].nil?
+    session[:user_id].present?
   end
 
+  def log_in_as user
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: "123456", remember_me: "1")
+    post login_path, params: { session: { email:        user.email,
+                                          password:     password,
+                                          remember_me:  remember_me } }
+  end
 end
