90610bfdc77210f8dc64c193ac775b8f4b4f74ee
ueihgnurt <aikawa.yuuto@gmail.com>
2020-07-14 16:35:09 +0700

    chapter_11 full

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save{email.downcase!}
-  validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255},
-            format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
-            uniqueness: true
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  attr_accessor :remember_token, :activation_token
+
+  scope :activated, ->{where activated: true}
+
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
+  validates     :name, presence: true, length: {maximum: 50}
+  validates     :email, presence: true, length: {maximum: 255},
+                format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+                uniqueness: true
+  validates     :password, presence: true, length: {minimum: 6}, allow_nil: true
+
   has_secure_password
   def self.digest string
     if ActiveModel::SecurePassword.min_cost
@@ -24,13 +30,32 @@ class User < ApplicationRecord
     update(remember_digest: User.digest(remember_token))
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.blank?
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false unless digest
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update(remember_digest: nil)
+  end
+
+  def activate
+    update(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  =  User.new_token
+    self.activation_digest =  User.digest(activation_token)
   end
 end
